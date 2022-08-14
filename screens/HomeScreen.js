069b1851da72794a29da57d99e4a9724661bc96f
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2022-08-14 18:34:10 +0530

    implemented maps functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../slices/navSlice";
 
 const HomeScreen = () => {
+	const dispatch = useDispatch();
+
 	return (
 		<SafeAreaView style={tw`bg-white h-full`}>
 			<View style={tw`p-5`}>
@@ -17,6 +23,40 @@ const HomeScreen = () => {
 						uri: "https://links.papareact.com/gzs",
 					}}
 				/>
+				<GooglePlacesAutocomplete
+					styles={{
+						container: {
+							flex: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					nearbyPlacesAPI="GooglePlacesSearch"
+					debounce={400}
+					placeholder="Where from?"
+					onPress={(data, details = null) => {
+						// 'details' is provided when fetchDetails = true
+						// console.log(data, details);
+						dispatch(
+							setOrigin({
+								location: details.geometry.location,
+								description: data.description,
+							})
+						);
+
+						dispatch(setDestination(null));
+					}}
+					returnKeyType={"search"}
+					fetchDetails={true}
+					minLength={2}
+					enablePoweredByContainer={false}
+					query={{
+						key: GOOGLE_MAPS_APIKEY,
+						language: "en",
+					}}
+				/>
+
 				<NavOptions />
 			</View>
 		</SafeAreaView>
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -27,7 +27,8 @@ export const { setOrigin, setDestination, setTravelTimeInformation } =
 
 // Selectors
 export const selectOrigin = (state) => state.nav.origin;
-export const selectDestination = (state) => state.nav.origin;
-export const selectTravelTimeInformation = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+	state.nav.travelTimeInformation;
 
 export default navSlice.reducer;
