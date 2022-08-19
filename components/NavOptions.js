de3c5baa4409af5dc7769cfbb956ea5e33ed8cac
pavansarvaiya58 <pavansarvaiya58@gmail.com>
2022-08-19 17:18:53 +0530

    implemented directions in map

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -34,7 +34,8 @@ const NavOptions = () => {
 				<TouchableOpacity
 					onPress={() => navigation.navigate(item.screen)}
 					style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
-					disabled={!origin}>
+					// disabled={!origin}
+				>
 					<View style={tw`${!origin && "opacity-20"}`}>
 						<Image
 							style={{ width: 120, height: 120, resizeMode: "contain" }}
